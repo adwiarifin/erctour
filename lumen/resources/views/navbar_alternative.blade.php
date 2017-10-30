2b778f3a1935bcae5299596c7e688923399b3795
Adwi Arifin <adwiarifin@gmail.com>
2017-10-30 13:05:12 +0700

    delete views at lumen's resource

--- lumen/resources/views/navbar_alternative.blade.php
+++
@@ -1,2 +0,0 @@
-                            <li><a class="home-nav" href="{{url('/')}}">Home</a></li>
-                            <li><a class="car-nav" href="{{url('/rentcar')}}">Rent Car</a></li>
